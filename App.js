3638047a75091b2fcc541c85970caa25d3d16505
Tohrak <alexis.dofus.compte@gmail.com>
2019-05-28 18:13:14 +0200

    Add input text
    *Compilation bug
    *Add input text

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
+//import Demofile from 'demofile';
+import { Alert, Button, ScrollView, StyleSheet, Text,TextInput, View, AppRegistry, Image } from 'react-native';
 
 
 export default class App extends React.Component {
@@ -8,22 +9,79 @@ export default class App extends React.Component {
       uri: "https://steamcdn-a.akamaihd.net/steam/subs/54029/header_586x192.jpg?t=1544227353"
     };
     return (
-      <View style={styles.container}>
-        <Image source={pic} style={styles.firstImg}/>
-        <View style={styles.textZone}>
-          <Text>My first application</Text>
-          <Welcome name='TorAk' />
+      //Page principale
+      <View style={{flex: 1}}>
+
+        //Bandeau haut
+        <View style={styles.bandeau}>
+          <View style={styles.textZone}>
+            //<Text> Bandeau Haut</Text>
+          </View>
         </View>
+
+        //Container centrale
+        <View style={styles.container}>
+          <ScrollView>
+            <Image source={pic} style={styles.firstImg}/>
+            <Image source={pic} style={styles.firstImg}/>
+            <Image source={pic} style={styles.firstImg}/>
+            <View style={styles.textZone}>
+              /*<Text>My first application</Text>
+              <Welcome name='TorAk' />
+              <UsernameInput/>*/
+            </View>
+            <Image source={pic} style={styles.firstImg}/>
+            <Image source={pic} style={styles.firstImg}/>
+            <Image source={pic} style={styles.firstImg}/>
+          </ScrollView>
+        </View>
+
+        //Bandeau bas
+        <View style={styles.bandeau}>
+          <View style={styles.textZone}>
+            //<Text> Bandeau Bas </Text>
+          </View>
+        </View>
+
+
       </View>
-    /*  <View style={{flex: 1}}>
-        <View style={{flex: 3, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 6, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 9, backgroundColor: 'steelblue'}} />
-      </View>*/
+      /**/
     );
   }
 }
 
+//Renvoie un textinput ainsi qu'un boutton
+class UsernameInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  _onPressButton(){
+    Alert.alert('Username send, please wait !');
+  }
+
+  render(){
+    return(
+      <View style={{padding: 5}}>
+          <TextInput
+            style={{height: 20}}
+            placeholder="Enter username to check !"
+            onChangeText={(text) => this.setState({text})}
+          />
+          <View style={styles.buttonContainer}>
+            <Button
+              onPress = {this._onPressButton}
+              title="Send username"
+            />
+          </View>
+      </View>
+    )
+  }
+}
+
+//Renvoie une chaine de charactère comprenant la chaîne de caractère
+//donnée en param
 class Welcome extends React.Component{
   constructor(props){
     super(props);
@@ -49,8 +107,19 @@ class Welcome extends React.Component{
 }
 
 const styles = StyleSheet.create({
-  container: {
+  bandeau: {
     flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'powderblue',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  container: {
+    flex: 8,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'stretch',
     justifyContent: 'center',
